5c7dc5ea63b16be97d256590914f57fc5fe84b46
MikhaelaMedrano <medranomikhaela@gmail.com>
2021-11-12 02:00:44 +0800

    UploadRunningMan

---
+++ RunningMan/RunningMan/Assets/Scripts/Start.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite btnSlide;
+    public UnityEvent btnClick;
+    void Awake()
+    {
+        if (btnClick == null) { btnClick = new UnityEvent();}
+    }
+
+    // Update is called once per frame
+    void OnMouseSlides()
+    {
+        GetComponent<SpriteRenderer>().sprite = btnSlide;
+    }
+}
